cda33ecf52e9c8ce77be4f6e1c7e2b08ecdebc43
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2022-11-28 10:15:12 +0000

    envirnment variables script

---
+++ environment-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Make sure to well preserve this env variables as it contains sensitive data.
+# Make sure to restrict access to this file.
+
+DNS_DOMAIN="test.domain.test.fe.domain";
+DNS_ZONE="XXXXXX";
+LISTENER_ID="XXXXXX";
+REGION="eu-west-0"
+BASE_ENDPOINT="prod-cloud-ocb.orange-business.com"
+USER_NAME="user.name"
+USER_PASSWORD='pass.word'
+DOMAIN_NAME="OCB000XXXX"
+PROJECT_ID="XXXXXXX"
+GLOBAL_PROJECT_ID="XXXXXX"
+ELB_EIP="X.X.X.X"
+CERTIFICATE_NAME="Certificate-name"
+TOKEN_HEADER_PREFIX='X-Subject-Token: '
